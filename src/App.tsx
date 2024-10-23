618ab616bfa09c024d295dabdb07ac63a7e90fa7
Maxime TRICOCHE <maxime.trche@gmail.com>
2024-10-23 16:07:53 +0200

    [FEAT] : Display the buttons for change pokemons

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,36 @@
+import { useState } from "react";
+
 import PokemonCard from "./components/PokemonCard";
 
 import "./styles/App.css";
 
 function App() {
+	const [pokemonIndex, setPokemonIndex] = useState(0);
+	console.log(pokemonIndex);
+	const handleClickPrevious = () => {
+		setPokemonIndex(pokemonIndex - 1);
+	};
+	const handleClickNext = () => {
+		setPokemonIndex(pokemonIndex + 1);
+	};
+
 	return (
 		<div>
-			<PokemonCard pokemon={pokemonList[0]} />
+			<PokemonCard pokemon={pokemonList[pokemonIndex]} />
+			{pokemonIndex > 0 ? (
+				<button type="button" onClick={handleClickPrevious}>
+					Précédent
+				</button>
+			) : (
+				""
+			)}
+			{pokemonIndex < pokemonList.length - 1 ? (
+				<button type="button" onClick={handleClickNext}>
+					Suivant
+				</button>
+			) : (
+				""
+			)}
 		</div>
 	);
 }
@@ -14,6 +39,18 @@ const pokemonList = [
 	{
 		name: "bulbasaur",
 		imgSrc: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+	},
+	{
+		name: "charmander",
+		imgSrc: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
+	},
+	{
+		name: "squirtle",
+		imgSrc: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
+	},
+	{
+		name: "pikachu",
+		imgSrc: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
 	},
 	{
 		name: "mew",
